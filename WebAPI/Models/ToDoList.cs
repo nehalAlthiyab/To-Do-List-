efbddf82c830cbcc4b14c61176a1058a470d4e57
Nehal Althiyab <nehal1997.n1@gmail.com>
2019-12-24 09:15:14 +0300

    add authentcstion

---
+++ WebAPI/Models/ToDoList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class ToDoList
+    {
+
+        [Key]
+        public int Id { get; set; }
+        public string work { get; set; }
+        [DisplayFormat(DataFormatString = "{dd/MM/yyyy}",ApplyFormatInEditMode = true)]
+        public DateTime dateFrom { get; set; }
+        [DisplayFormat(DataFormatString = "{dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime dateTo { get; set; }
+        public int completed { get; set; }
+    }
+}
